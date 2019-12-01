c45e7ccb7dda25cacaf8816059deb007a307b5a2
Martha MacGregor <macgregm@tcd.ie>
2019-12-01 10:19:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,14 @@
 function basicTeenager(age) {
   if (age >=13 && age <=19)
-  {console.log("You are a teenager!")}
+  {return("You are a teenager!")}
 
 }
 
 function teenager(age) {
-
+  if (age >=13 && age <=19)
+  {return("You are a teenager!")}
+else 
+{return("You are not a teenager")}
 }
 
 function ageChecker(age) {
